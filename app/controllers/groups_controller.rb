96e11a8d7dfca430799c67c49c15b8e727ca5a46
chizuru <czr.shinoda@gmail.com>
2019-05-26 20:21:10 +0900

    ルーティングの設定、ビューの素材をダウンロード/アプリケーションのが一頭フォルダに配置

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,23 @@
 class GroupsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
   def create
-  end
-
-  def edit
-    if current_user.edit(user_params)
-      redirect_to root_path
-    else 
-      render :edit
-    end
-  end
-
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else 
-      render :edit
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
     end
   end
 
   private
-
-  def user_params
-    params.require(:user).permitt(:name, :email)
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
