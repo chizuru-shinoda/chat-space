be020711580716182b47c9effbe4eac56108b327
chizuru <czr.shinoda@gmail.com>
2019-06-10 14:57:09 +0900

    メッセージ送信の非同期化

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,58 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper__message">
+                    <div class="upper__messag__user-name">
+                      ${message.user_name}
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${content}
+                      ${img}
+                    </P>
+                  </div>
+                </div>`
+  return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      scrollBottom();
+
+      function scrollBottom(){
+        var target = $('.message').last();
+        var position = target.offset().top + $('.messages').scrollTop();
+        $('.messages').animate({
+          scrollTop: position
+        }, 300, 'swing');
+      }
+    })
+
+    .fail(function(data){
+      alert('error');
+    })
+
+    .always(function(data){
+      $('.form-submit').prop('disabled', false);
+    })
+  })
+});
